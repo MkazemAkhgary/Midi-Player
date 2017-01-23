9eaeb000c2e1f5263aba7f3c87a76855f10331a5
MkazemAkhgary <l.van.beethoven9@gmail.com>
2017-01-23 23:20:35 +0330

    Optimized animations

--- MidiApp/ContentControls/PlayerControl.xaml.cs
+++ MidiApp/ContentControls/PlayerControl.xaml.cs
@@ -60,23 +60,35 @@ namespace MidiApp.ContentControls
             Action beginAnimation = () =>
             {
                 var correction = selection.ActualWidth;
-                if (selection.ActualWidth + center > track.ActualWidth) correction -= center;
+                if (selection.ActualWidth + center > track.ActualWidth)
+                    correction -= center; // prevent going outside of bounds.
 
-                var ratio = correction / track.ActualWidth;
+                var ratio = correction/track.ActualWidth;
+
+                if (animation.To == ratio)
+                    return; // to prevent animation freeze when there is no need for animation change.
+
                 animation.To = ratio;
 
-                storyboard.Begin();
+                storyboard.Stop(border);
+                storyboard.Begin(border, true);
             };
-            // todo needs further optimization.
-            slider.ValueChanged += (o, args) => beginAnimation();
+            
+            slider.ValueChanged += async (o, args) =>
+            {
+                if(thumb.IsDragging) return; // drag delta will handle animation.
+                await Task.Delay(10); // wait for value changes
+                beginAnimation();
+            };
             thumb.DragStarted += (o, args) => beginAnimation();
             thumb.DragDelta += (o, args) => beginAnimation();
             thumb.DragCompleted += async (o, args) =>
             {
-                if (args.Canceled) await Task.Delay(10); // wait for value changes.
+                if (args.Canceled) await Task.Delay(10); // wait for value changes
                 beginAnimation();
             };
 
+            // at startup initialize values.
             var init = selection.ActualWidth;
             if (selection.ActualWidth + center > track.ActualWidth) init -= center;
             border.Tag = init / track.ActualWidth;
